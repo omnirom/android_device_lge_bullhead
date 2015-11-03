1786f9924a27a47481ed777ca093afb68ae03b53
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-02 19:48:11 -0500

    bullhead: setup omni makefiles
    Change-Id: I8a1020a38d888098fdf5562baedd747933120c76

---
+++ omni_bullhead.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file includes all definitions that apply to ALL bullhead devices, and
+# are also specific to bullhead devices
+#
+# Everything in this directory will become public
+
+# Include omni board-specific configurations
+-include device/lge/bullhead/BoardConfigOmni.mk
+
+# Inherit base AOSP device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit APNs list
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 960x640
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5x
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0 MDB08M 2353240 release-keys"
+
