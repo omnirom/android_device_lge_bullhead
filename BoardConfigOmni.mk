dcac5203ed5902545178ea3a6cd74cd8f7528b66
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-02 19:55:28 -0500

    bullhead: set TWRP configuration
    Change-Id: I4c08c9f80290775573d585687a86e5599ebb9349

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -5,3 +5,13 @@ TARGET_KERNEL_CONFIG := bullhead_defconfig
 
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
