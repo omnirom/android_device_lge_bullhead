969dfa1c22000b4a18946a3df29a8f7d17039fbc
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-17 18:25:40 -0500

    bullhead: use 1080x814 for new bootanimation
    Change-Id: I9016be890f138cbdbe26078645132b28b12ed109

--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -13,6 +13,8 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 1080x608
 
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
