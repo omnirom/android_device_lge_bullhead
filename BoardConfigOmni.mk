16299be961d75a539be18b457a436f4409d4fc53
Jake Whatley <jacob.whatley91@gmail.com>
2016-01-19 17:21:06 -0500

    bullhead: exclude SuperSU from TWRP
    Change-Id: I008f948f5a476a351e6661c3e25e4f8ed0cc2f8c

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -24,3 +24,4 @@ BOARD_HAS_NO_REAL_SDCARD := true
 TW_INCLUDE_CRYPTO := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_EXCLUDE_SU := true
