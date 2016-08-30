4399e5c5713d3904f9a4f69af89f40f6e8ad9696
Jake Whatley <jacob.whatley91@gmail.com>
2016-08-31 01:05:23 +0200

    adapt to Omni build structure
    Change-Id: I6e760eae482f6d4320b39ece4b22e67a9b613b77

---
+++ omni_bullhead.mk
@@ -0,0 +1,21 @@
+# Exclude Live Wallpapers
+TARGET_EXCLUDE_LIVEWALLPAPERS := true
+
+# Inherit base AOSP device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit APNs list
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.0/NRD90M/3085278:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90M 3085278 release-keys"
