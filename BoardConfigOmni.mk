c9170bde9fbaf5aa7ae37afe19320140a549c596
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-01 00:04:53 +0100

    audio_effects: use device's audio_effects.conf
    This reverts commit eda7240617144eacbd212d47f28a3bddf16583b4.
    
    Change-Id: I966870f82ba1dcc3f334f660c4e6850e51a1fb44

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -7,6 +7,9 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
+# Ignore DSPManager's audio_effects config file
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
