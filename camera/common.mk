46659ed61cd9fda1f58b60a6668a874f35168d47
Jacob Whatley <jake@whatley.co>
2017-09-05 15:27:51 +0000

    camera: add kernel headers dependencies

--- camera/common.mk
+++ camera/common.mk
@@ -1,6 +1,9 @@
 common_deps :=
 kernel_includes :=
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
