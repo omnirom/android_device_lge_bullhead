91b7e432ca4738b9637d3946a6db8dca6c7ededd
Jacob Whatley <jake@whatley.us>
2018-04-20 22:26:28 +0200

    bullhead: update build fingerprint and description
    Change-Id: Ib2580e591440c231a5b08e223927c182d31be535

--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -14,6 +14,6 @@ PRODUCT_MODEL := Nexus 5X
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR6.170623.013 4283548 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
 
-BUILD_FINGERPRINT := "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys" \
+BUILD_FINGERPRINT := "google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys" \
